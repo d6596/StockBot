7706257e46933b57d80b4cf3772ed32cd9dcba5e
d6596 <40365025+d6596@users.noreply.github.com>
2018-06-20 01:28:12 +0900

    StockBot

---
+++ KiwoomStock/KiwoomCode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiwoomCode
+
+{
+    public enum Log
+    {
+        API조회,
+        API결과,
+        STotockAPI콜백,
+        주식봇,
+        에러
+    }
+
+    class KOAErrorCode
+    {
+
+    }
+
+    public class KOACode
+    {
+
+    }
+
+    public  struct OrderType
+    {
+
+    }
+
+    public readonly static OrderType[] orderType = new OrderType[6];
+
+    public struct HogaGb
+    {
+
+    }
+    public readonly static HogaGb[] hogaGb = new HogaGb[13];
+
+    public  struct MarketCode
+    {
+        
+    }
+
+    public readonly static MarketCode[] marketCode = new MarketCode[9];
+
+    static KOACode()
+    {
+
+
+    }
+    class Error
+    {
+
+    }
+    class KiwoomCode
+    {
+
+    }
+}
---
+++ Util/SingleTon.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Reflection;
+
+namespace StockBot.Util
+{
+    public class SingleTon<T> where T : class
+    {
+        private static object _synobj = new object();
+        private static volatile T _instance = null;
+
+        public static T GetInstance
+        {
+            get
+            {
+                if (_instance == null)  CreateInstance();
+                return _instance;
+            }
+        }
+
+        private static void CreateInstance()
+        {
+
+        }
+    }
+
+}
+
